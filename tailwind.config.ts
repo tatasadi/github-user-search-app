40eec38b2c2c1f81b244ce57f2d10cc36f67ee97
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-03-27 14:42:18 +0100

    add colors & typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "bright-blue": "#0079FF",
+        "slate-gray": "#697C9A",
+        "cool-blue": "#4B6A9B",
+        charcoal: "#2B3442",
+        "lightest-blue": "#F6F8FF",
+        "off-white": "#FEFEFE",
+        "dark-blue": "#141D2F",
+        "deep-blue": "#1E2A47",
+      },
+    },
+  },
+  plugins: [],
 }
 export default config
